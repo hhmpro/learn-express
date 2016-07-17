494779a1735bc469f58f1b509137d76cbc7ea9b7
hhmpro <240050497@qq.com>
2016-07-17 20:31:35 +0800

    app.on('mount',function(parent){})

--- app.mountpath.js
+++ app.mountpath.js
@@ -7,17 +7,27 @@ admin.get('/', function(req, res) {
   console.log('admin.mountpath:', admin.mountpath); // ['/adm*n','/manager']
   res.send('Admin Homepage');
 });
+admin.on('mount', function(parent) {
+  console.log('Admin mounted, parent is:', parent);
+});
 
 var secret = express();
 secret.get('/', function(req, res) {
   console.log('secret.mountpath:', secret.mountpath); // /secr*t
   res.send('Secret Page'); // 必须调用res.send()方法，页面才不会一直加载
 });
+secret.on('mount', function(parent) {
+  console.log('Secret mounted, parent is:', parent);
+});
+
 
 var salary = express();
 salary.get('/', function(req, res) {
-  console.log('salary.mountpath:', salary.mountpath);
+  console.log('salary.mountpath:', salary.mountpath); // [ '/salary', '/money' ]
   res.send('Salary Page');
+});
+salary.on('mount', function(parent) {
+  console.log('Salary mounted, parent is:', parent);
 });
 
 secret.use(['/salary', '/money'], salary);
@@ -30,3 +40,13 @@ var port = 3000;
 app.listen(port, function() {
   console.log('app listening on port: ' + port);
 });
+
+// Salary mounted, parent is: function(req, res, next) {
+//   app.handle(req, res, next);
+// }
+// Secret mounted, parent is: function(req, res, next) {
+//   app.handle(req, res, next);
+// }
+// Admin mounted, parent is: function(req, res, next) {
+//   app.handle(req, res, next);
+// }
