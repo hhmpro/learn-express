25924565af4116d04ea4231b6b754093a3a8c87f
hhmpro <240050497@qq.com>
2016-07-18 12:19:29 +0800

    test app.path

---
+++ app.path.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express(),
+  blog = express(),
+  blogAdmin = express();
+
+app.use('/blog', blog);
+blog.use('/admin', blogAdmin);
+
+console.log(app.path()); // ''
+console.log(blog.path()); // '/blog'
+console.log(blogAdmin.path()); // '/blog/admin'
